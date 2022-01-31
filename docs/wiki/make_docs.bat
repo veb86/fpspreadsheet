cdcb3f8505dffe7d6aa00134a1547fe337ecdff3
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2022-01-31 11:32:56 +0000

    fpspreadsheet: Update help files for v1.14. Add instructions how to build them.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn/components/fpspreadsheet@8193 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- docs/wiki/make_docs.bat
+++ docs/wiki/make_docs.bat
@@ -10,5 +10,7 @@ wikiget --page=TsWorksheetGrid --page=TsWorksheetChartSource
 
 echo.
 echo Converting wiki to chm...
-wikiconvert --format=%FMT% --css=css/wiki.css --root="FPSpreadsheet wiki pages" --title="FPSpreadsheet wiki pages (offline version, created %DATE%)" --chm="..\fpspreadsheet-wiki.chm" wikixml/FPSpreadsheet.s00.xml wikixml/FPSpreadsheet=3A_Examples*.xml wikixml/FPSpreadsheet=3A_List_of_formulas*.xml wikixml/RPN_Formulas*.xml wikixml/FPSpreadsheet_tutorial=3A_Writing*.xml wikixml/TsWorksheet*.xml
+
+wikiconvert --format=%FMT% --css=css/wiki.css --root="FPSpreadsheet wiki pages" --title="FPSpreadsheet wiki pages (offline version, created %DATE%)" --chm="..\fpspreadsheet-wiki.chm" wikixml/FPSpreadsheet.s00.xml wikixml/FPSpreadsheet=3A_Examples.s0300.xml wikixml/FPSpreadsheet=3A_List_of_formulas.s03000.xml wikixml/RPN_Formulas_in_FPSpreadsheet.u03g00.xml wikixml/FPSpreadsheet_tutorial=3A_Writing_a_mini_spreadsheet_application.s000c0000000.xml wikixml/TsWorksheetGrid.k08.xml
+
 set FMT=
