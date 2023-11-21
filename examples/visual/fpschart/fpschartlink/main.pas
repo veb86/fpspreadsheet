6f059e64280c408965621d10b48cc1955f3b39d6
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2023-11-21 22:03:20 +0000

    fpspreadsheet: Add chart link demo
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn/components/fpspreadsheet@9036 8e941d3f-bd1b-0410-a28a-d453659cc2b4

---
+++ examples/visual/fpschart/fpschartlink/main.pas
@@ -0,0 +1,59 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  TAGraph,
+  fpSpreadsheet, fpsTypes, fpsOpenDocument,
+  fpSpreadsheetCtrls, fpSpreadsheetGrid,  fpSpreadsheetChart;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Chart1: TChart;
+    Splitter1: TSplitter;
+    sWorkbookSource1: TsWorkbookSource;
+    sWorksheetGrid1: TsWorksheetGrid;
+    procedure FormCreate(Sender: TObject);
+  private
+    sChartLink: TsWorkbookChartLink;
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+const
+  FILE_NAME = '../../../other/chart/bars.ods';
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  fn: String;
+begin
+  fn := ExpandFileName(FILE_NAME);
+
+  sWorkbookSource1.FileFormat := sfOpenDocument;
+  if FileExists(fn) then
+    sWorkbookSource1.Filename := fn;
+
+  sChartLink := TsWorkbookChartLink.Create(self);
+  sChartLink.Chart := Chart1;
+  sChartLink.WorkbookSource := sWorkbookSource1;
+  sChartLink.WorkbookChartIndex := 0;
+end;
+
+end.
+
