c20ae208df3df18637c4b48aaa7acfb5021a5b20
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2024-09-21 22:02:17 +0000

    FPSpreadsheet: Rename components in chartlink demo. Update charts after arrow keys in filename combobox.
    git-svn-id: https://svn.code.sf.net/p/lazarus-ccr/svn/components/fpspreadsheet@9461 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- examples/visual/fpschart/fpschartlink/main.pas
+++ examples/visual/fpschart/fpschartlink/main.pas
@@ -18,22 +18,22 @@ type
   TForm1 = class(TForm)
     btnBrowse: TButton;
     btnOpen: TButton;
-    Chart1: TChart;
-    ComboBox1: TComboBox;
-    Label1: TLabel;
-    ListChartSource1: TListChartSource;
-    Memo1: TMemo;
-    OpenDialog1: TOpenDialog;
+    Chart: TChart;
+    cbFileNames: TComboBox;
+    lblFileNames: TLabel;
+    ListChartSource: TListChartSource;
+    Memo: TMemo;
+    OpenDialog: TOpenDialog;
     Panel1: TPanel;
     Panel2: TPanel;
     Splitter1: TSplitter;
-    sWorkbookSource1: TsWorkbookSource;
-    sWorksheetGrid1: TsWorksheetGrid;
+    sWorkbookSource: TsWorkbookSource;
+    sWorksheetGrid: TsWorksheetGrid;
     procedure btnBrowseClick(Sender: TObject);
     procedure btnOpenClick(Sender: TObject);
-    procedure ComboBox1CloseUp(Sender: TObject);
+    procedure cbFileNamesSelect(Sender:TObject);
     procedure FormCreate(Sender: TObject);
-    procedure sWorkbookSource1Error(Sender: TObject; const AMsg: String);
+    procedure sWorkbookSourceError(Sender: TObject; const AMsg: String);
   private
     FDir: String;
     sChartLink: TsWorkbookChartLink;
@@ -61,13 +61,13 @@ procedure TForm1.btnBrowseClick(Sender: TObject);
 var
   fn: String;
 begin
-  fn := ExpandFileName(Combobox1.Text);
-  OpenDialog1.InitialDir := ExtractFilePath(fn);
-  OpenDialog1.FileName := '';
-  if OpenDialog1.Execute then
+  fn := ExpandFileName(cbFileNames.Text);
+  OpenDialog.InitialDir := ExtractFilePath(fn);
+  OpenDialog.FileName := '';
+  if OpenDialog.Execute then
   begin
-    Combobox1.Text := OpenDialog1.FileName;
-    LoadFile(OpenDialog1.FileName);
+    cbFileNames.Text := OpenDialog.FileName;
+    LoadFile(OpenDialog.FileName);
   end;
 end;
 
@@ -75,19 +75,21 @@ procedure TForm1.btnOpenClick(Sender: TObject);
 var
   fn: String;
 begin
-  if FileNameIsAbsolute(Combobox1.Text) then
-    fn := Combobox1.Text
+  if FileNameIsAbsolute(cbFileNames.Text) then
+    fn := cbFileNames.Text
   else
-    fn := FDir + Combobox1.Text;
+    fn := FDir + cbFileNames.Text;
   LoadFile(fn);
 end;
 
-procedure TForm1.ComboBox1CloseUp(Sender: TObject);
+procedure TForm1.cbFileNamesSelect(Sender:TObject);
+var
+  fn: String;
 begin
-  if ComboBox1.ItemIndex > -1 then
+  if cbFileNames.ItemIndex > -1 then
   begin
-    Combobox1.Text := FDir + Combobox1.Items[Combobox1.ItemIndex];
-    LoadFile(Combobox1.Text);
+    fn := FDir + cbFileNames. Items[cbFileNames.ItemIndex];
+    LoadFile(fn);
   end;
 end;
 
@@ -102,24 +104,24 @@ begin
     FindAllFiles(L, FDir, '*.xlsx;*.ods', false);
     for i := 0 to L.Count-1 do
       L[i] := ExtractFileName(L[i]);
-    Combobox1.Items.Assign(L);
+    cbFileNames.Items.Assign(L);
   finally
     L.Free;
   end;
 
   {$IF LCL_FullVersion >= 2020000}
-  ComboBox1.TextHint := 'Enter or select file name';
+  cbFileNames.TextHint := 'Enter or select file name';
   {$IFEND}
   if ParamCount > 0 then
   begin
-    Combobox1.Text := ParamStr(1);
-    LoadFile(Combobox1.Text);
+    cbFileNames.Text := ParamStr(1);
+    LoadFile(cbFileNames.Text);
   end;
 end;
 
-procedure TForm1.sWorkbookSource1Error(Sender: TObject; const AMsg: String);
+procedure TForm1.sWorkbookSourceError(Sender: TObject; const AMsg: String);
 begin
-  Memo1.Lines.Add(AMsg);
+  Memo.Lines.Add(AMsg);
 end;
 
 procedure TForm1.LoadFile(AFileName: String);
@@ -128,7 +130,7 @@ var
   fn: String;
   i: Integer;
 begin
-  Memo1.Lines.Clear;
+  Memo.Lines.Clear;
 
   fn := ExpandFileName(AFileName);
   if not FileExists(fn) then
@@ -139,19 +141,19 @@ begin
 
   ext :=Lowercase(ExtractFileExt(fn));
   if ext = '.ods' then
-    sWorkbookSource1.FileFormat := sfOpenDocument
+    sWorkbookSource.FileFormat := sfOpenDocument
   else
-    sWorkbookSource1.Fileformat := sfOOXML;
-  sWorkbookSource1.Filename := fn;
+    sWorkbookSource.Fileformat := sfOOXML;
+  sWorkbookSource.Filename := fn;
 
-  for i := 1 to sWorksheetGrid1.Worksheet.GetLastRowIndex+1 do
-    sWorksheetGrid1.AutoRowHeight(1);
+  for i := 1 to sWorksheetGrid.Worksheet.GetLastRowIndex+1 do
+    sWorksheetGrid.AutoRowHeight(1);
 
   FreeAndNil(sChartLink);
 
   sChartLink := TsWorkbookChartLink.Create(self);
-  sChartLink.Chart := Chart1;
-  sChartLink.WorkbookSource := sWorkbookSource1;
+  sChartLink.Chart := Chart;
+  sChartLink.WorkbookSource := sWorkbookSource;
   sChartLink.WorkbookChartIndex := 0;
 end;
 
